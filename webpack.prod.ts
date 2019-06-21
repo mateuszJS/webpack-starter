605e797205dc3509c48aab07a3d992dfcbed5111
Mateusz Walendzik <mate.walendzik@gmail.com>
2019-06-22 01:26:35 +0200

    add comment explaining weird names in webpack production config file

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,5 +1,13 @@
 const webpackMerge = require('webpack-merge')
 const webpackCommon = require('./webpack.common.ts')
+/*
+  That prefix in const's names ('webpack') was added because TS recognizes this file as a script.
+  TS assume that scripts have a global scoping, so there is no way to declare
+  the same consts in this file and in the webpack.dev.ts. There are only two options to fix that:
+  1) change the name of consts
+  2) add import or export, then TS will recognize this file as a module
+  (module can't add consts to global scope)
+*/
 
 module.exports = webpackMerge(webpackCommon, {
   mode: 'production'
